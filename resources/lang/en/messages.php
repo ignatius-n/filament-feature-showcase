0e0a5693c2d0e40ad80de370e5a4ecf20d098ec0
dezaelDev <emmanuelfernandez.developer@gmail.com>
2026-05-11 16:54:06 -0300

    feat: add package translation support

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'button' => [
+        'title' => "What's New",
+    ],
+
+    'header' => [
+        'version' => 'Version :version',
+        'title' => "What's New",
+        'description' => "See what's changed across all versions",
+    ],
+
+    'version' => [
+        'latest' => 'latest',
+        'fallback_title' => 'Version :version',
+    ],
+
+    'actions' => [
+        'dismiss' => "Got it, let's go!",
+        'close' => 'Close',
+    ],
+];
